6abe9b2a0c7b1d7ccbc6054e231758463b8aae59
luncheon <takeuck+github@gmail.com>
2021-06-20 17:36:40 +0900

    フォントサブセット化

---
+++ font/subset.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const Fontmin = require("fontmin");
+
+new Fontmin()
+  .src(path.resolve(__dirname, "過充電FONT くれよん/KajudenFont-Crayon.ttf"))
+  .use(Fontmin.glyph({ text: " 0123456789+-=はじめるさんすうシャウトこたえがわかったらさけんでね" }))
+  .use(Fontmin.ttf2woff2())
+  .dest(path.resolve(__dirname, "../src/assets/"))
+  .run((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
